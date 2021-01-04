242ab972ada09c41c1d3735b9f75ee348f0c4ecf
Samy Djemaï <53857555+SamyDjemai@users.noreply.github.com>
2021-01-04 03:09:44 +0100

    Removed useless comments in PHP components

--- components/scripts.php
+++ components/scripts.php
@@ -1,16 +1,8 @@
-<!-- ============================================================== -->
-<!-- All Jquery -->
-<!-- ============================================================== -->
 <script src="js/jquery.min.js"></script>
-<!-- Bootstrap popper Core JavaScript -->
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<!--Custom JavaScript -->
 <script src="js/custom.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<!-- ============================================================== -->
-<!-- This page plugins -->
-<!-- ============================================================== -->
 <script type="text/javascript">
     $('a').on('click', function(event) {
         var $anchor = $(this);
